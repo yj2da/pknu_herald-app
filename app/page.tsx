7db4bd3f758e65944173793115ce600c1a832f13
오예진 <oyjcat@naver.com>
2026-05-25 22:49:28 +0900

    Critical Fix: Radical UI overhaul and responsive scaling to ensure changes are visible

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import NewsFeed from '@/components/NewsFeed';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-100 dark:bg-black flex items-center justify-center py-10">
-      <IPhoneFrame>
-        <NewsFeed />
-      </IPhoneFrame>
+    <main className="min-h-screen bg-[#F2F2F7] dark:bg-black flex items-center justify-center p-4 sm:p-8">
+      <div className="scale-[0.85] sm:scale-100 origin-center transition-transform duration-500">
+        <IPhoneFrame>
+          <NewsFeed />
+        </IPhoneFrame>
+      </div>
     </main>
   );
 }
--- components/IPhoneFrame.tsx
+++ components/IPhoneFrame.tsx
@@ -6,22 +6,22 @@ interface IPhoneFrameProps {
 
 const IPhoneFrame: React.FC<IPhoneFrameProps> = ({ children }) => {
   return (
-    <div className="relative mx-auto border-gray-900 bg-gray-900 border-[10px] rounded-[3.5rem] h-[720px] w-[360px] shadow-2xl overflow-visible flex-shrink-0">
-      {/* Notch */}
-      <div className="w-[120px] h-[30px] bg-gray-900 top-0 left-1/2 -translate-x-1/2 absolute z-20 rounded-b-[1.5rem]"></div>
+    <div className="relative mx-auto border-[#1c1c1e] bg-[#1c1c1e] border-[12px] rounded-[3.5rem] h-[740px] w-[360px] shadow-[0_50px_100px_-20px_rgba(0,0,0,0.3)] overflow-visible flex-shrink-0 outline outline-1 outline-[#3a3a3c]">
+      {/* Notch / Dynamic Island style */}
+      <div className="w-[110px] h-[32px] bg-black top-2 left-1/2 -translate-x-1/2 absolute z-30 rounded-full shadow-inner border border-gray-800"></div>
       
       {/* Buttons */}
-      <div className="h-[40px] w-[3px] bg-gray-900 absolute -left-[13px] top-[120px] rounded-l-lg"></div>
-      <div className="h-[40px] w-[3px] bg-gray-900 absolute -left-[13px] top-[170px] rounded-l-lg"></div>
-      <div className="h-[60px] w-[3px] bg-gray-900 absolute -right-[13px] top-[140px] rounded-r-lg"></div>
+      <div className="h-[40px] w-[3px] bg-[#1c1c1e] absolute -left-[15px] top-[120px] rounded-l-lg border-l border-gray-700"></div>
+      <div className="h-[40px] w-[3px] bg-[#1c1c1e] absolute -left-[15px] top-[170px] rounded-l-lg border-l border-gray-700"></div>
+      <div className="h-[80px] w-[3px] bg-[#1c1c1e] absolute -right-[15px] top-[140px] rounded-r-lg border-r border-gray-700"></div>
       
       {/* Screen Content */}
-      <div className="rounded-[2.8rem] overflow-hidden w-full h-full bg-white dark:bg-black relative shadow-inner">
-        <div className="h-full overflow-y-auto scrollbar-hide pt-10">
+      <div className="rounded-[2.8rem] overflow-hidden w-full h-full bg-white relative">
+        <div className="h-full overflow-y-auto scrollbar-hide pt-12">
           {children}
         </div>
         {/* Home Indicator */}
-        <div className="absolute bottom-1.5 left-1/2 -translate-x-1/2 w-1/3 h-1 bg-gray-200 dark:bg-gray-800 rounded-full opacity-50"></div>
+        <div className="absolute bottom-2 left-1/2 -translate-x-1/2 w-[120px] h-[5px] bg-black/10 rounded-full z-20"></div>
       </div>
     </div>
   );
